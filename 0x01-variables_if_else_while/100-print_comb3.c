1e5caedb06ac7c8625fe12cd8920374ad35ed57f
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-12 07:58:22 -0700

    Print combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i != j && j > i)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+			}
+		}
+	}
+	return (0);
+}
