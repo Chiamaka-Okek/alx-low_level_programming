d8be255df82ca8bb457c6fe96b079f61f6145707
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-17 04:47:01 -0700

    Print lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,15 @@
 /**
  * print_alphabet - prints alphabets in lowercase followed by newline
  *
- * Return: Always 0
+ * Return: void
  */
 void print_alphabet(void)
 {
 	char alpha;
 
-	for (alpha = 'a'; && alpha <= 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
 	_putchar('\n');
-	return (0);
 }
