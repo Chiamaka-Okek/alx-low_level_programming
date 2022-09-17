cb57d0e20db31b5bf4252dd3c4f076728765f6bb
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-17 04:53:40 -0700

    Print lowercase alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (alpha = 'a'; alpha = 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
