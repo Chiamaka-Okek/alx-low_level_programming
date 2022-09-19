02177c4e3da222827e55f5219217cf6f366fef18
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-18 18:57:56 -0700

    Compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int)
+{
+	if (a < '0' || a > '0')
+	{
+		return (1);
+	}
+}
