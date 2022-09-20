951a490842bb6ed6dc7b9d2bc6704a0f6fbc292c
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-19 18:14:04 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,22 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src)
+	int i = 0;
+	int srclen = 0;
+
+	while (dest[i] != '\0')
 	{
-		*dest++ = *src++;
+		dest[i++] = '\0';
 	}
+	i = 0;
+	while (src[srclen] != '\0')
+	{
+		srclen++;
+	}
+	for (i = 0; i < srclen; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[srclen] = '\0';
 	return (dest);
 }
