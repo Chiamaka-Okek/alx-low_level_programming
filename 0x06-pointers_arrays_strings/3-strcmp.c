4d2d155b312695d115c05154ee1624b5facaff9e
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-22 13:36:38 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1[0] < s2[0])
+	int i = 0;
+
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		return (-1);
+		if (s1[i] != s2[1])
+		{
+			return (s1 - s2);
+			i++;
+		}
 	}
-	else if (s1[0] > s2[0])
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
