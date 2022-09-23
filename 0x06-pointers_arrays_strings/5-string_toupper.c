eb5f30029874561968a31ae497e9e3cd226ecc63
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-22 19:06:47 -0700

    Change lowercase letters to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,11 @@ char *string_toupper(char *a)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (a[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (a[i] >= 'a' && a[i] <= 'z')
 		{
-			str[i] -= 32;
+			a[i] -= 32;
 		}
 		i++;
 	}
