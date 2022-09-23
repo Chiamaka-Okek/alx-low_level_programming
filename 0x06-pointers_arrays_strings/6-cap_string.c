d4e5b3ad806931c1f26f8fa2264810177073d7db
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-22 19:25:52 -0700

    Capitalize all string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main'h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @a: input string
+ *
+ * Return: Always 0
+ */
+char *cap_string(char *a)
+{
+	int i = 0;
+	int j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i))
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (i == 0 && s[i] >= 97 && s[i] <= 122)
+			{
+				s[x] -= 32;
+			}
+			if (s[x - 1] == a[j])
+			{
+				if ((*(s + i) >= 97) && (*(s + i) <= 122))
+				{
+					*(s + i) -= 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (a);
+
+}
