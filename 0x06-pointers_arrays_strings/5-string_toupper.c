21fa9b5d7446841c9bbc79c866166deac9e9be80
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-22 19:00:54 -0700

    Change lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase to uppercase
+ *
+ * @a: input string
+ *
+ * Return: Always 0
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++
+	}
+	return (a);
+}
