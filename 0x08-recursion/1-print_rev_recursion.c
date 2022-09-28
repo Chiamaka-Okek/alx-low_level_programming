ac2a9de9e4079d6655f22ef3ac40b96bd0b625d5
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-28 13:57:40 -0700

    Print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: pointer to string
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] > '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,1 +1,3 @@
 0-puts_recursion.c file prints a string followed by a new line.
+
+1-print_rev_recursion.c file prints a string in reverse
