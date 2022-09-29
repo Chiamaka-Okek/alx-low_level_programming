690bcfc6d0f742d81407caddd62039d157aea435
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-09-29 11:14:06 -0700

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,42 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion(int n) - prints the natural square root of a number
+ * _sqrt_recursion - prints the natural square root of a number
  *
  * @n: input integer
  *
  * Return: natural square root else return -1
  */
 int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt(1, n));
+}
+
+/**
+ * _sqrt - checks for square root
+ *
+ * @a: number used to check
+ * @b: square root of the number
+ *
+ * Return: square root
+ */
+int _sqrt(int a, int b)
+{
+	if (a > b)
+	{
+		return (-1);
+	}
+	else if (a * a == b)
+	{
+		return (a);
+	}
+	return (_sqrt(a + 1, b));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -7,3 +7,7 @@
 3-factorial.c file returns  the factorial of a given number
 
 4-pow_recursion.c file returns the value of x raised to the power of y
+
+5-sqrt_recursion.c file returns the natural square root of a number
+
+6-is_prime_number.c file checks for prime numbers
