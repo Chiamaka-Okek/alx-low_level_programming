ba3629fe0e60e570b890d66ba60d94b5a204e471
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-10-04 11:22:35 -0700

    Dynamic memory allocation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *) malloc(sizeof(int * width));
+		arr[i] = (int *) malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
 			return (NULL);
