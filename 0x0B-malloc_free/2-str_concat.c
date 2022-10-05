b754313a83bcd9c96a88b03e744b1ea9028e6098
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-10-04 19:34:26 -0700

    Dynamic memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,45 @@
 #include "main.h"
-#include <stdlib'h>
+#include <stdlib.h>
 
 /**
  * str_concat - concatenates two strings
  *
- * @s1: pointer to input string
- * @s2: pointer to string to be appended
+ * @s1: input string 1
+ * @s2: string to be concantenated
  *
- * Return: pointer to newly allocated space or NULL on failure
+ * Return:pointer to newly allocated space for concatenated strings
  */
-char * str_concat(char *s1, char *s2)
+char *str_concat(char *s1, char *s2);
+{
+	char *str_concat;
+	int i;
+	int j = 0;
+	int length = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i] || s2[i]; i++)
+	{
+		length++;
+	}
+	str_concat = malloc(sizeof(char) * length);
+	if (str_concat == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		str_concat[j++] = s1[i];
+	}
+	for (i = 0; s2[i]; i++)
+	{
+		str_concat[j++] = s2[i];
+	}
+	return (str_concat);
+}
