f735ebd9545247568765e3b785837ec84acac4cc
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-10-06 18:28:46 -0700

    Dynamic memory allocation

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _calloc - allocates memory for an array using malloc
@@ -11,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *memory;
-	unsigned int mem, i;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
