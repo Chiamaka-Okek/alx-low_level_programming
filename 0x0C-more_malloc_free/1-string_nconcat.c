f5ef81122a492cb6c544612ca2d281955bc37ecf
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-10-06 17:25:56 -0700

    Dynamic memory allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,35 +6,25 @@
  *
  * @s1: pointer to first string
  * @s2: pointer to second string
+ * @n: the number of bytes to be considered for concatenation
  *
  * Return: a pointer to the concatenated string or NULL if it fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1 = 0, l2 = 0;
-	unsigned int i;
+	unsigned int i, l1 = 0, l2 = 0;
 	char *memory;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	while (s1[l1])
-	{
 		l1++;
-	}
 	while (s2[l2])
-	{
 		l2++;
-	}
 	if (n >= l2)
-	{
 		n = l2;
-	}
 	memory = malloc(l1 + n + 1)
 	if (memory == NULL)
 	{
