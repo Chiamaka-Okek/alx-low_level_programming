bbdf9bf8c3302e7735db6acf76cb21d4184dc9c4
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-10-12 14:48:15 -0700

    Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,12 @@ void array_iterator(int *array, size_t size, void (*action)(int)
 {
 	int i;
 
-	if (array)
+	if (array == NULL || action == NULL)
 	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
 	}
 }
