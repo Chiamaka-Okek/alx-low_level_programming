8aa3ca25239e0ecac5b1b11848a43111580c709c
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-10-13 17:21:25 -0700

    Variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ *
+ * @format: list of variety types of arguments
+ *
+ * Return:nothing
+ */
+void print_all(const char * const format, ...)
+{
+	char *value;
+	unsigned int i = 0;
+	va_list args;
+
+	va_start(args, format);
+	while (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", (char) va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", (float) va_arg(args, double));
+				break;
+			case 's':
+				value = va_arg(args, char *);
+				if (value == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				else
+				{
+					printf("%s", value);
+					break;
+				}
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') && format[(i + 1)] != '\0')
+		{
+			printf(",");
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,3 +1,7 @@
 0-sum_them_all.c file sums all of its parameters
 
 1-print_numbers.c file prints numbers followed by a new line
+
+2-print_strings.c file prints strings followed by a new line
+
+3-print_all.c file prints anything
