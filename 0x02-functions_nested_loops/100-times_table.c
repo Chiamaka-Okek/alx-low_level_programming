14ca292bd66f2c72014014628d43df42a3da9114
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-11-22 09:16:13 -0800

    Fix function length

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,8 @@ void print_times_table(int n)
 		return;
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
+		_putchar('0');
+		for (j = 1; j <= n; j++)
 		{
 			k = i * j;
 			format(k);
@@ -35,31 +36,28 @@ void print_times_table(int n)
  */
 void format(int k)
 {
-	if (i <= n && j < n)
+	if (k < 10)
 	{
-		if (k < 10)
-		{
-			_putchar(k + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(' ');
-		}
-		else if (k >= 10 && k < 100)
-		{
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-		}
-		else
-		{
-			_putchar((k / 100) + '0');
-			_putchar(((k / 100) / 10) + '0');
-			_putchar((k % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(k + '0');
+	}
+	else if (k >= 10 && k < 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((k / 10) + '0');
+		_putchar(k % 10) + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar((k / 100) + '0');
+		_putchar(((k / 100) /10) + '0');
+		_putchar((k % 10) + '0');
 	}
 }
