ccb2e9335f52031d23aec32ce13eafb765d9f91e
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-11-22 06:56:02 -0800

    Fix the character printed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 			if (k < 10)
 			{
 				_putchar(k + '0');
-				if (j < 9)
+				if (j < 9 && i < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -29,7 +29,7 @@ void times_table(void)
 			{
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-				if (j < 9)
+				if (j < 9 && i < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
