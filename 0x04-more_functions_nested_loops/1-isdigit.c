33681374fd643190f39389acf7d26bdae0903327
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-12-22 08:55:14 -0800

    Fix error

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -15,7 +15,7 @@ int _isdigit(int c)
 	for (i = '0'; i <= '9'; i++)
 	{
 		if (c == i)
-			return (0);
+			return (1);
 	}
 	return (0);
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,13 +13,8 @@ void print_most_numbers(void)
 	for (i = '0'; i <= '9'; i++)
 	{
 		if (i == 2 || i == 4)
-		{
-			break;
-		}
-		else
-		{
-			_putchar(i);
-		}
+			continue;
+		_putchar(i);
 	}
 	_putchar('\n');
 }
