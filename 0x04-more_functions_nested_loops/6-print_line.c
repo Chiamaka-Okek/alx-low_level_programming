777fa71970db16f404fee898902f92a892858d8f
Chiamaka-Okek <chiamakarokeke@gmail.com>
2023-01-04 10:55:13 -0800

    Fix error

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,12 +12,12 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-		_putchar('\n');
-	if (n > 0)
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		if (n > 0)
 			_putchar('_');
+		else
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
